189fce5a037dcf68f767d8aa41a90c6f61b13206
Melissa Wong <melissa.wong89@gmail.com>
2015-05-14 19:42:47 -0700

    Favicon, Adjusted H1 for SEO

--- wp-content/themes/glk/home.php
+++ wp-content/themes/glk/home.php
@@ -7,17 +7,17 @@ $post = simple_fields_get_post_group_values(get_the_id(), "Post", false, 2);
 $post = $post[0];
 ?>
 <div class="container center">
-	<h1>Eggs and Such</h1>
-	<h3>Served Until 2pm.</h3> 
+	<h2>Eggs and Such</h2>
+	<h4>Served Until 2pm.</h4> 
 	<p>Eggs come prepared as described on the menu. An alternative of scrambled is offered.</p>
 	<hr>
 </div>
 <div class="container">
 	<div class="grid">
 		<div class="grid-1-1">
-			<h2>Soft Scrambled Eggs $12.00</h2>
+			<h3>Soft Scrambled Eggs $12.00</h3>
 			<p>Smoked chedder on thick-sliced toast with bacon or avocado, side of greens</p>
-			<h4>with bacon and avocado add $2.00</h4>
+			<h5>with bacon and avocado add $2.00</h5>
 		</div>
 	</div>
 </div>
