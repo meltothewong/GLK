ed2e6621c4953266b470f7f00b453ddc3d8369bc
Melissa Wong <melissa.wong89@gmail.com>
2015-06-09 13:29:28 -0700

    Social icons

--- wp-content/themes/glk/parts/footer.php
+++ wp-content/themes/glk/parts/footer.php
@@ -2,6 +2,12 @@
             <div class="container">
                 <?php Starkers_Utilities::get_template_parts( array( 'parts/info' ) ); ?>
             </div>
+            <div class="contianer social center">
+	        	<a href="https://twitter.com/GrandLakeKitch" title="Grand Lake Kitchen on Twitter"><span class="icon icon-twitter"></span></a>
+	        	<a href="https://www.facebook.com/GrandLakeKitchen" title="Grand Lake Kitchen on Facebook"><span class="icon icon-facebook"></span></a>
+	        	<a href="https://instagram.com/grandlakekitchen/" title="Grand Lake Kitchen on Instagram"><span class="icon icon-instagram"></span></a>
+	        	<a href="http://www.yelp.com/biz/grand-lake-kitchen-oakland" title="Grand Lake Kitchen on Yelp"><span class="icon icon-yelp"></span></a>
+            </div>
 		</footer>
 		<script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js'></script>
         <script src="/js/build/production.js"></script>
